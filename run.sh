fc1fcca4706a172c6c458c2de4066540f9b455b3
liudahai <1111426@greatwall.com.cn>
2020-09-10 17:28:54 +0800

    shell脚本修改

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-VARR=X
 #容器名称
 CONTAINT_NAME=springio-demo
 #镜像名称
@@ -10,7 +9,7 @@ REMOTE_IMAGE_NAME=192.168.71.3:6000/${IMAGE_NAME}:${VERSION}
 RUN_CONTAINT=`docker ps | grep ${CONTAINT_NAME}`
 echo ${RUN_CONTAINT}
 #判断容器是否启动
-if [ ${VARR}${RUN_CONTAINT} = $VARR ]
+if [ -n "${RUN_CONTAINT}" ]
 then
 	#暂停容器
 	docker stop ${CONTAINT_NAME}
@@ -19,29 +18,29 @@ then
 	docker rm ${CONTAINT_NAME}
 	echo "删除容器${CONTAINT_NAME}成功"
 	#删除镜像
-	docker rmi ${IMAGE_NAME}
-	echo "删除镜像${IMAGE_NAME}成功"
+	docker rmi ${REMOTE_IMAGE_NAME}
+	echo "删除镜像${REMOTE_IMAGE_NAME}成功"
 else
 	#判断容器是否存在
 	EXIST_CONTAINT=`docker ps -a | grep ${CONTAINT_NAME}`
-	if [ ${VARR}${EXIST_CONTAINT} = ${VARR} ]
+	if [ -n "${EXIST_CONTAINT}" ]
 	then
 		#删除容器
 		docker rm ${CONTAINT_NAME}
 		echo "删除容器${CONTAINT_NAME}成功"
 		#删除镜像
-		docker rmi ${IMAGE_NAME}
-		echo "删除镜像${IMAGE_NAME}成功"
+		docker rmi ${REMOTE_IMAGE_NAME}
+		echo "删除镜像${REMOTE_IMAGE_NAME}成功"
 	else
 		#查看镜像是否存在
-		EXIST_IMAGE=`docker images | grep ${IMAGE_NAME}`
-		echo ${EXIST_IMAGE}
+		EXIST_IMAGE=`docker images | grep ${REMOTE_IMAGE_NAME}`
+		echo ${REMOTE_IMAGE_NAME}
 		#存在则删除镜像
-		if [ ${VARR}${EXIST_IMAGE} = ${VARR} ]
+		if [ -n "${EXIST_IMAGE}" ]
 		then
 			#删除镜像
-			docker rmi ${IMAGE_NAME}
-			echo "删除镜像${IMAGE_NAME}成功"
+			docker rmi ${REMOTE_IMAGE_NAME}
+			echo "删除镜像${REMOTE_IMAGE_NAME}成功"
 		fi
 	fi
 fi
