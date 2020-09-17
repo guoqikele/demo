290ab44057bf66cfe9015527c0e6ece9566af1a5
liudahai <1111426@greatwall.com.cn>
2020-09-17 19:12:28 +0800

    mysql路径修改

--- src/main/java/com/gwi/hns/demo/Interceptor/SessionHandlerInterceptor.java
+++ src/main/java/com/gwi/hns/demo/Interceptor/SessionHandlerInterceptor.java
@@ -12,21 +12,21 @@ import com.gwi.hns.demo.constant.ResponseConstant;
 import com.gwi.hns.demo.vo.ResponseEntity;
 
 public class SessionHandlerInterceptor implements HandlerInterceptor {
-	//在目标方法执行之前运行此方法
+    //在目标方法执行之前运行此方法
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    	HttpSession session = request.getSession();
+        HttpSession session = request.getSession();
         String loginUser = (String) session.getAttribute("loginUser");
-        if(StringUtils.isEmpty(loginUser)){
+        if (StringUtils.isEmpty(loginUser)) {
             //说明用户未登陆
-        	ResponseEntity<String> resp = new ResponseEntity<String>();
-        	resp.setCode(ResponseConstant.FORBIDDEN.getCode());
-        	resp.setErrorMsg(ResponseConstant.FORBIDDEN.getMsg());
-        	JsonMapper mapper = new JsonMapper();
-        	//设置编码格式
+            ResponseEntity<String> resp = new ResponseEntity<String>();
+            resp.setCode(ResponseConstant.FORBIDDEN.getCode());
+            resp.setErrorMsg(ResponseConstant.FORBIDDEN.getMsg());
+            JsonMapper mapper = new JsonMapper();
+            //设置编码格式
             response.setCharacterEncoding("UTF-8");
             response.setContentType("application/json;charset=UTF-8");
-        	response.getWriter().print(mapper.writeValueAsString(resp));
+            response.getWriter().print(mapper.writeValueAsString(resp));
             return false;
         }
         return true;
--- src/main/java/com/gwi/hns/demo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gwi/hns/demo/handler/GlobalExceptionHandler.java
@@ -11,12 +11,12 @@ import com.gwi.hns.demo.vo.ResponseEntity;
 //@ControllerAdvice
 class GlobalExceptionHandler {
 
-	@ExceptionHandler(value = Exception.class)
-	public ResponseEntity<Object> defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-		ResponseEntity<Object> rep = new ResponseEntity<Object>();
-		rep.setCode(ResponseConstant.SYSTEM_ERROR.getCode());
-		rep.setErrorMsg(e.getCause().toString());
-		return rep;
-	}
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ResponseEntity<Object> rep = new ResponseEntity<Object>();
+        rep.setCode(ResponseConstant.SYSTEM_ERROR.getCode());
+        rep.setErrorMsg(e.getCause().toString());
+        return rep;
+    }
 
 }
